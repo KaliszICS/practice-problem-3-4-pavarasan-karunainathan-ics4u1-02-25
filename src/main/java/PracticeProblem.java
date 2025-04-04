aca9f436f11a710ee802595793e44a2c50149ce9
pavarasan-karunainathan-ics4u1-02-25 <166336911+pavarasan-karunainathan-ics4u1-02-25@users.noreply.github.com>
2025-04-04 14:09:50 +0000

    Problems Complete

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,31 @@
 public class PracticeProblem {
+	static int[] arr;
+	static boolean[] inSeq = new boolean[1000000];
+	static int recamanNum(int n){
+		if(n == 0){
+			return 0;
+		}
 
-	public static void main(String args[]) {
-
+		int a = recamanNum(n-1);
+		if(a-n > 0 && !inSeq[a-n]){
+			arr[n-1] = a-n;
+			inSeq[a-n] = true;
+			return a-n;
+		}else{
+			arr[n-1] = a+n;
+			inSeq[a+n] = true;
+			return a+n;
+		}
+	}
+	static int[] recaman(int n){
+		if(n < 0){
+			return new int[0];
+		}
+		arr = new int[n];
+		inSeq = new boolean[1000000];
+		recamanNum(n);
+		return arr;
 	}
 
-	public static void q1() {
-		//Write question 1 code here
-	}
-
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
+	public static void main(String args[]){}
 }
